e04b055815337682a86fcfaa58721cacdb7ecd33
MuXi <muxi@ibantang.com>
2020-04-21 11:44:24 +0800

    注册AsmRockCodeTransform

--- asmrock/src/main/groovy/com/muxi/asmrock/AsmRock.java
+++ asmrock/src/main/groovy/com/muxi/asmrock/AsmRock.java
@@ -1,5 +1,8 @@
 package com.muxi.asmrock;
 
+import com.android.build.gradle.AppExtension;
+import com.muxi.asmrock.transform.AsmRockCodeTransform;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -7,6 +10,24 @@ public class AsmRock implements Plugin<Project> {
     @Override
     public void apply(Project project) {
 
-        System.out.println("Hello world!AsmRock!");
+        //AppExtension 就是 Android项目中的build.gradle下android对应的配置类:
+        // android {
+        //    compileSdkVersion 29
+        //    buildToolsVersion "29.0.2"
+        //    defaultConfig {
+        //        applicationId "com.muxi.gradleasmrock"
+        //        minSdkVersion 23
+        //        targetSdkVersion 29
+        //        versionCode 1
+        //        versionName "1.0"
+        //    }
+        //}
+        // AppExtension是Android的gradle插件提供的，这就是为什么我们要在asm_rock的module中引用:
+        // implementation 'com.android.tools.build:gradle:3.6.2'
+        // implementation 'com.android.tools.build:gradle-api::3.6.2'
+        AppExtension appExtension = (AppExtension) project.getProperties().get("android");//获取android属性相关的配置类
+        if (appExtension != null) {
+            appExtension.registerTransform(new AsmRockCodeTransform(project));//获取到Android项目的配置类，Transform就是注册到AppExtension中的。
+        }
     }
 }
---
+++ asmrock/src/main/groovy/com/muxi/asmrock/transform/AsmRockCodeTransform.java
@@ -0,0 +1,12 @@
+package com.muxi.asmrock.transform;
+
+import com.quinn.hunter.transform.HunterTransform;
+
+import org.gradle.api.Project;
+
+public class AsmRockCodeTransform extends HunterTransform {
+
+    public AsmRockCodeTransform(Project project) {
+        super(project);
+    }
+}
