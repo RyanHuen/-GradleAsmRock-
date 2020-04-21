f82d215afc8510443cbbac4509d205f964afe1d2
MuXi <muxi@ibantang.com>
2020-04-21 11:47:57 +0800

    fix error

--- asmrock/src/main/groovy/com/muxi/asmrock/transform/AsmRockCodeTransform.java
+++ asmrock/src/main/groovy/com/muxi/asmrock/transform/AsmRockCodeTransform.java
@@ -1,12 +1,24 @@
 package com.muxi.asmrock.transform;
 
+import com.android.build.api.transform.Context;
+import com.android.build.api.transform.TransformException;
+import com.android.build.api.transform.TransformInput;
+import com.android.build.api.transform.TransformOutputProvider;
 import com.quinn.hunter.transform.HunterTransform;
 
 import org.gradle.api.Project;
+
+import java.io.IOException;
+import java.util.Collection;
 
 public class AsmRockCodeTransform extends HunterTransform {
 
     public AsmRockCodeTransform(Project project) {
         super(project);
+    }
+
+    @Override
+    public void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs, TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
+        super.transform(context, inputs, referencedInputs, outputProvider, isIncremental);
     }
 }
